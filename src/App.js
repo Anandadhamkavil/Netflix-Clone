efeebd5a7fde70843d8a7614ca97a5a8f58889dc
Anandadhamkavil <anandadhamkavil123@gmail.com>
2023-02-28 16:54:16 +0530

    netflex_clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Row from './components/Row'
+import request from './request'
+import Banner from './components/Banner';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav/>
+      <Banner/>
+     <Row isPoster={true} title='NetFlix Originals' fetchUrl={request.fetchNetflixOriginals}/> 
+     <Row title='Trending' fetchUrl={request.fetchTrending}/> 
+     <Row title='Top Rated' fetchUrl={request.fetchTopRated}/> 
+     <Row title='Action' fetchUrl={request.fetchActionMovies}/> 
+     <Row title='Comedy' fetchUrl={request.fetchComedyMovies}/> 
+     <Row title='Horror' fetchUrl={request.fetchHorrorMovies}/> 
+     <Row title='Romance' fetchUrl={request.fetchRomanceMovies}/> 
+     <Row title='Documentary' fetchUrl={request.fetchDocumentaries}/> 
+
     </div>
   );
 }
